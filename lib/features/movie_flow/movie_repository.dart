eac1706eeb15ad613eb05eafce414c42d08be63d
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-10-13 12:31:49 +0300

    Created a MovieRepository for the repository-service pattern.

---
+++ lib/features/movie_flow/movie_repository.dart
@@ -0,0 +1,64 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:show_recommender_app/core/environment_variables.dart';
+import 'package:show_recommender_app/features/movie_flow/genre/genre_entity.dart';
+import 'package:show_recommender_app/features/movie_flow/result/movie_entity.dart';
+import 'package:show_recommender_app/main.dart';
+
+abstract class MovieRepository {
+  Future<List<GenreEntity>> getMovieGenres();
+  Future<List<MovieEntity>> getRecommendedMovies(
+    double rating,
+    String date,
+    String genreIds,
+  );
+}
+
+class TMDBMovieRepository implements MovieRepository {
+  final Dio dio;
+  TMDBMovieRepository({required this.dio});
+
+  @override
+  Future<List<GenreEntity>> getMovieGenres() async {
+    final response = await dio.get(
+      "genre/movie/list",
+      queryParameters: {
+        "api_key": apiKey,
+        "language": "en-US",
+      },
+    );
+    final results = List<Map<String, dynamic>>.from(response.data["genres"]);
+    final genres =
+        results.map((element) => GenreEntity.fromMap(element)).toList();
+
+    return genres;
+  }
+
+  @override
+  Future<List<MovieEntity>> getRecommendedMovies(
+      double rating, String date, String genreIds) async {
+    final response = await dio.get(
+      "discover/movie",
+      queryParameters: {
+        "api_key": apiKey,
+        "language": "en-US",
+        "sort_by": "popularity.desc",
+        "include_adult": true,
+        "vote_average.gte": rating,
+        "page": 1,
+        "release_date.gte": date,
+        "with_genres": genreIds,
+      },
+    );
+    final results = List<Map<String, dynamic>>.from(response.data["results"]);
+    final movies =
+        results.map((element) => MovieEntity.fromMap(element)).toList();
+    return movies;
+  }
+}
+
+final movieRepositoryProvider = Provider<MovieRepository>((ref) {
+  final dio = ref.watch(dioProvider);
+  return TMDBMovieRepository(dio: dio);
+});
