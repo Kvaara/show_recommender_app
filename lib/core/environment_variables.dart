dd8da842b2bf24a1666509ac5a832025a9a989c6
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2022-01-13 12:53:12 +0200

    Removed a redundant StatefulWidget from main.dart.

--- lib/core/environment_variables.dart
+++ lib/core/environment_variables.dart
@@ -1,4 +1,4 @@
 // Used for getting an environment variable named tmdbAPI and assigning it to apiKey
 // Example: flutter run --dart-define=tmdbAPI=example125qdasdexample
 // const String apiKey = String.fromEnvironment("tmdbAPI");
-const String apiKey = String.fromEnvironment("tmdbAPI");
+const String apiKey = "1ca0c0cb4c625e027dd32c26bba1170d";
